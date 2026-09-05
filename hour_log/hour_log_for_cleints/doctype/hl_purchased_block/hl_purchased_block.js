ec608ab38b3df7202f21f460ecbe7b4493b8253a
Neha Fathima <nehaputhur443@gmail.com>
2026-09-05 14:41:20 +0530

    fix: doctype fix

---
+++ hour_log/hour_log_for_cleints/doctype/hl_purchased_block/hl_purchased_block.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, neha@enfono.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("HL Purchased Block", {
+// 	refresh(frm) {
+
+// 	},
+// });
